821b22a8c1c0d73f6bb590083ccfd0e6ce1ecd6a
tass2t <brunog90@gmail.com>
2022-12-05 17:31:07 +0100

    added option to guid

--- galaxy/Utils/guid.js
+++ galaxy/Utils/guid.js
@@ -6,10 +6,42 @@ export default class GUID {
     this.galaxy = new Galaxy();
     this.instance = new dat.GUI();
 
-    this.instance
-      .add(this.galaxy, "starCount")
-      .min(100)
-      .max(10000000)
-      .step(500);
+    console.log(this.galaxy.camera.instance);
+    const cameraFolder = this.instance.addFolder("Camera");
+    cameraFolder
+      .add(this.galaxy.camera.instance.position, "x")
+      .min(-50)
+      .max(50)
+      .step(5);
+
+    cameraFolder
+      .add(this.galaxy.camera.instance.position, "y")
+      .min(-50)
+      .max(50)
+      .step(5);
+
+    cameraFolder
+      .add(this.galaxy.camera.instance.position, "z")
+      .min(-50)
+      .max(50)
+      .step(5);
+
+    console.log(this.galaxy.control.instance);
+    const controlFolder = this.instance.addFolder("Control");
+    controlFolder
+      .add(this.galaxy.control.instance, "autoRotate")
+      .name("auto rotation");
+    controlFolder
+      .add(this.galaxy.control.instance, "autoRotateSpeed")
+      .min(0.0)
+      .max(10)
+      .step(0.5)
+      .name("auto rotation speed");
+    controlFolder
+      .add(this.galaxy.control.instance, "enableZoom")
+      .name("enable zoom");
+    controlFolder
+      .add(this.galaxy.control.instance, "enableRotate")
+      .name("enable rotation");
   }
 }
