89d6ee49bd8dc466f894fee113d6f2b9ed82d188
tass2t <brunog90@gmail.com>
2022-12-05 18:12:03 +0100

    added options to guid

--- galaxy/Utils/guid.js
+++ galaxy/Utils/guid.js
@@ -6,41 +6,43 @@ export default class GUID {
     this.galaxy = new Galaxy();
     this.instance = new dat.GUI();
 
-    console.log(this.galaxy.camera.instance);
-    const cameraFolder = this.instance.addFolder("Camera");
-    cameraFolder
+    this.makeFolders();
+  }
+
+  makeFolders() {
+    // CAMERA FOLDER
+    this.cameraFolder = this.instance.addFolder("Camera");
+    this.cameraFolder
       .add(this.galaxy.camera.instance.position, "x")
       .min(-50)
       .max(50)
       .step(5);
-
-    cameraFolder
+    this.cameraFolder
       .add(this.galaxy.camera.instance.position, "y")
       .min(-50)
       .max(50)
       .step(5);
-
-    cameraFolder
+    this.cameraFolder
       .add(this.galaxy.camera.instance.position, "z")
       .min(-50)
       .max(50)
       .step(5);
 
-    console.log(this.galaxy.control.instance);
-    const controlFolder = this.instance.addFolder("Control");
-    controlFolder
+    // CONTROL FOLDER
+    this.controlFolder = this.instance.addFolder("Control");
+    this.controlFolder
       .add(this.galaxy.control.instance, "autoRotate")
       .name("auto rotation");
-    controlFolder
+    this.controlFolder
       .add(this.galaxy.control.instance, "autoRotateSpeed")
       .min(0.0)
       .max(10)
       .step(0.5)
       .name("auto rotation speed");
-    controlFolder
+    this.controlFolder
       .add(this.galaxy.control.instance, "enableZoom")
       .name("enable zoom");
-    controlFolder
+    this.controlFolder
       .add(this.galaxy.control.instance, "enableRotate")
       .name("enable rotation");
   }
